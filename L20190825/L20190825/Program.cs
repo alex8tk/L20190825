483ba811bba43a09310018b3fc8c1ad1c85c450f
ds <asdf@gmail.com>
2019-11-24 19:23:31 +0200

    1

--- L20190825/L20190825/Program.cs
+++ L20190825/L20190825/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace L20190825
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			string name = Console.ReadLine();
+			Console.WriteLine(name);
+			Console.ReadLine();
+			Console.WriteLine();
 		}
 	}
 }
+
